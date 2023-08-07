ac7a5869f2482b27f30edbbdc8858d0a2691cf6d
Abnorma1yNorma1 <StrengManGrey@gmail.com>
2023-08-07 15:06:16 +0300

    room in gradle update

--- app/src/main/java/com/example/taskmaster/MainActivity.kt
+++ app/src/main/java/com/example/taskmaster/MainActivity.kt
@@ -3,20 +3,27 @@ package com.example.taskmaster
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.taskmaster.databinding.ActivityMainBinding
+import com.example.taskmaster.model.Task
 import com.example.taskmaster.repository.TaskRepository
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-//    private lateinit var taskRepository: TaskRepository
+    private lateinit var taskRepository: TaskRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-//        taskRepository = TaskRepository(TaskmasterApp.INSTANCE.database.taskDao())
-//        TaskmasterApp.INSTANCE.database.taskDao().incert()
+        taskRepository = TaskRepository(TaskmasterApp.INSTANCE.database.taskDao())
+        with (TaskmasterApp.INSTANCE.database.taskDao()){
+            insert(Task("do this", expirationDate = 1))
+            println(getAllTasks())
+            deleteAll()
+        }
+
+
     }
 }
